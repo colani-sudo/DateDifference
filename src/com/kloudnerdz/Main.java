c4e5d318d13c647f69aa2f49fce599c42ea36688
colani-sudo <matsenjwacolani@gmail.com>
2024-01-22 17:08:26 +0800

    initial commit

---
+++ src/com/kloudnerdz/Main.java
@@ -0,0 +1,12 @@
+package com.kloudnerdz;
+
+/** Write a program that calculates the age difference
+ * between two siblings born March 3rd, 1999 and
+ * September 20th, 2003
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
